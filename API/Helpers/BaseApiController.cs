b9fbe80feb14153d37e703d993a52cc2a580e56b
Pankaj Tiwari <ptiwari@appsnovel.com>
2024-08-23 11:46:48 +0530

    Upload files from CodeZen

---
+++ API/Helpers/BaseApiController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using newDay_project23.Helpers.Extensions;
+
+namespace newDay_project23.Helpers
+{
+    /// <summary>
+/// Base class of API controller
+/// </summary>
+    public class BaseApiController : ControllerBase
+    {
+        /// <summary>
+/// User id
+/// </summary>
+        protected Guid UserId { get => HttpContext.User.Identity.GetUserId(); }
+        /// <summary>
+/// Tenant id
+/// </summary>
+        protected Guid TenantId { get => HttpContext.User.Identity.GetTenantId(); }
+    }
+}
